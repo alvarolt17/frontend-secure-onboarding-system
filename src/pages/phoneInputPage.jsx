e941e446b814e9d16105783b1a803b76c881cb8f
bostang <bostangsteiitb2020@gmail.com>
2025-07-18 13:11:21 +0700

    integrasi dengan backend (login)

--- src/pages/phoneInputPage.jsx
+++ src/pages/phoneInputPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import './PhoneInput.css';
+import './phoneInputPage.css';
 import logo from '../assets/wondr-logo.png';
 import phoneIcon from '../assets/handphone.png';
 import indonesiaFlag from '../assets/flag.png';
